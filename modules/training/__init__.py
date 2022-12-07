13d9d6050f3656d2409bd2f72843a617d47b8b04
Lars Hauptmann <lhauptmann@eu-login-17.euler.ethz.ch>
2022-12-08 00:13:46 +0100

    fixed first_and_final not found error

--- modules/training/__init__.py
+++ modules/training/__init__.py
@@ -3,7 +3,7 @@ from .Metric import *
 from .train_utils import * 
 from .Trainer import * 
 
-from .Validator import Validator
+from .Validator import *
 from .FocalLoss import *
 from .WeightedBCELoss import *
 from .WeightedFocalLoss import *
