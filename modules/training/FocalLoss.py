eee9847a4d7871a64b21bfede3ee03b611c65867
mmaninetti <marco.maninettimm@gmail.com>
2022-12-08 13:31:51 +0100

    changes to focal loss

--- modules/training/FocalLoss.py
+++ modules/training/FocalLoss.py
@@ -5,12 +5,12 @@ import torch.nn.functional as F
 #from modules.training.ComputeLossWeights import ComputeLossWeights
 import numpy as np
 
-ALPHA = 0.8
+ALPHA = 0.25
 GAMMA = 2
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 #Primary unweighted version
-class FocalLoss(nn.Module):
+'''class FocalLoss(nn.Module):
     def __init__(self, weight=None, size_average=True):
         super(FocalLoss, self).__init__()
 
@@ -29,4 +29,24 @@ class FocalLoss(nn.Module):
         BCE_EXP = torch.exp(-BCE.to(device))
         focal_loss = alpha * (1-BCE_EXP.to(device))**gamma * BCE.to(device)
                        
-        return focal_loss.to(device)
+        return focal_loss.to(device)'''
+
+#As implemented here: https://amaarora.github.io/2020/06/29/FocalLoss.html#alpha-and-gamma
+#Reduction = None !!!
+class FocalLoss(nn.Module):
+    "Non weighted version of Focal Loss"
+    def __init__(self, alpha=ALPHA, gamma=GAMMA):
+        super(FocalLoss, self).__init__()
+        self.alpha = torch.tensor([alpha, 1-alpha]).to(device)
+        self.gamma = gamma
+
+    def forward(self, inputs, targets):
+    #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        BCE_loss = F.binary_cross_entropy(inputs.to(device), targets.to(device), reduction='none')
+        at = (self.alpha.gather(0, targets.type(torch.int64))).to(device)
+        pt = torch.exp(-BCE_loss.to(device))
+        F_loss = at*(1-pt.to(device))**self.gamma * BCE_loss.to(device)
+        return (torch.mean(F_loss)).to(device)
--- modules/training/WeightedFocalLoss.py
+++ modules/training/WeightedFocalLoss.py
@@ -10,7 +10,7 @@ WEIGHTS = torch.Tensor(np.array(weights))
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-ALPHA = 0.8
+ALPHA = 0.25
 GAMMA = 2
 
 #Primary unweighted version
@@ -62,10 +62,12 @@ GAMMA = 2
         return focal_loss'''
 
 class WeightedFocalLoss(nn.Module):
-    def __init__(self, weight=None, size_average=True):
+    def __init__(self, alpha = ALPHA, gamma=GAMMA,weight=None, size_average=True):
         super(WeightedFocalLoss, self).__init__()
+        self.alpha = torch.tensor([alpha, 1-alpha]).to(device)
+        self.gamma = gamma
 
-    def forward(self, inputs, targets, alpha=ALPHA, gamma=GAMMA, smooth=1):
+    def forward(self, inputs, targets, smooth=1):
         
         #comment out if your model contains a sigmoid or equivalent activation layer
         #inputs = F.sigmoid(inputs)       
@@ -79,9 +81,10 @@ class WeightedFocalLoss(nn.Module):
         #first compute binary cross-entropy 
         #print('targets:',targets,targets.size())
         #print(targets)
-        BCE_WEIGHT = F.binary_cross_entropy(inputs.to(device), targets.to(device), reduction='mean',weight=weights1.to(device)) #TODO: add the weights with beta here
-        BCE_unweight = F.binary_cross_entropy(inputs.to(device), targets.to(device), reduction='mean')
+        BCE_WEIGHT = F.binary_cross_entropy(inputs.to(device), targets.to(device), reduction='none',weight=weights1.to(device)) #TODO: add the weights with beta here
+        BCE_unweight = F.binary_cross_entropy(inputs.to(device), targets.to(device), reduction='none')
+        at = (self.alpha.gather(0, targets.type(torch.int64))).to(device)
         BCE_EXP = torch.exp(-BCE_unweight.to(device))
-        focal_loss = alpha * (1-BCE_EXP.to(device))**gamma * BCE_WEIGHT.to(device)
+        focal_loss = torch.mean(at * (1-BCE_EXP.to(device))**self.gamma * BCE_WEIGHT.to(device))
                         
         return focal_loss.to(device)
