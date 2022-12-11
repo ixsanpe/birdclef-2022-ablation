3d4b7ef9c7ace38e0f032d3cb8eb5d70b0bd95fd
fnestaas <94458061+fnestaas@users.noreply.github.com>
2022-12-11 17:48:55 +0100

    Update train_runnable.py

--- train_runnable.py
+++ train_runnable.py
@@ -32,9 +32,6 @@ from datetime import datetime
 DATA_PATH = config("DATA_PATH")
 SPEC_PATH = config('SPEC_PATH')
 OUTPUT_DIR = config("OUTPUT_DIR")
-SPLIT_PATH = config("SPLIT_PATH")
-AUGMENT_PATH  = config("AUGMENT_PATH")
-SPLIT_PATH_KFOLD = config("SPLIT_PATH_KFOLD")
 
 def parse_args():
     """
@@ -74,7 +71,7 @@ def parse_args():
     parser.add_argument('--model_name', type=str, default='efficientnet_b2')
     parser.add_argument('--InstanceNorm', type=s2b)
     parser.add_argument('--SimpleAttention', type=s2b, default='True')
-    parser.add_argument('--k_runs', type=int, default=1, help='Number of splits for cross validation. Default to 1. Current implementation only support 1 and 3')
+    parser.add_argument('--k_runs', type=int, choices=[1, 3], default=1, help='Number of splits for cross validation. Default to 1. Current implementation only support 1 and 3')
 
     # wandb stuff
     parser.add_argument('--wandb', type=s2b, default='True')
@@ -83,276 +80,281 @@ def parse_args():
 
     return parser.parse_args()
 
+def train(args, k=1):
+    experiment_name = args.experiment_name
+    # Take args defined in parse_args(): 
+    duration = args.duration 
+    max_duration = args.max_duration
+    n_splits = args.n_splits
+    test_split = args.test_split 
+    sr = args.sr 
+    offset_train = args.offset_train 
+    offset_val = args.offset_val
+
+    bs_train = args.batch_size_train
+    bs_val = args.batch_size_val
+    precompute = args.precompute
+    augs = args.augs
+    aug_prob = args.aug_prob
+
+    epochs = args.epochs 
+    learning_rate = args.learning_rate
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model_name = args.model_name
+
+    # Here, to use the args, define a dict
+    # containing losses available, and access them with the
+    # key args.loss
+
+    losses = {'BCELoss':nn.BCELoss(),'FocalLoss':FocalLoss(), 'WeightedBCELoss':WeightedBCELoss(),'WeightedFocalLoss':WeightedFocalLoss()}
+
+
+    criterion = losses[args.loss]
+
+    # Similarly define optimizers as losses
+    optimizers = {'Adam': Adam}
+    optimizer = optimizers[args.optimizer]
+
+    overlap = args.overlap
+    validate_every = args.validate_every
+    use_wandb = args.wandb
+    project_name = args.project_name 
+
+    N = args.N 
+
+    # From this point on, it is a regular train function
+    if N != -1:
+        warnings.warn(f'\n\nWarning! Using only {N} training examples!\n')
+
+    # Load data 
+    with open(f'{DATA_PATH}all_birds.json') as f:
+        birds = json.load(f)
+
+    num_classes = len(birds)
+
+    if args.k_runs == 1:
+        SPLIT_PATH = config("SPLIT_PATH")
+        df_train = pd.read_csv(f'{SPLIT_PATH}train_metadata.csv')[:N]
+        df_val = pd.read_csv(f'{SPLIT_PATH}val_metadata.csv')[:N]
+    elif args.k_runs == 3:
+        SPLIT_PATH_KFOLD = config("SPLIT_PATH_KFOLD")
+        df_train = pd.read_csv(f'{SPLIT_PATH_KFOLD}{k}/train_metadata.csv')[:N]
+        df_val = pd.read_csv(f'{SPLIT_PATH_KFOLD}{k}/val_metadata.csv')[:N]
+    else:
+        raise NotImplementedError(f'k-fold cross validation only implemented for k =1, 3 but got {k=}')
+
+    # Datasets, DataLoaders
+    if precompute:
+        if augs != '':
+            AUGMENT_PATH  = config("AUGMENT_PATH")
+            train_data = AugmentDataset(df_train, SPEC_PATH, AUGMENT_PATH, augmentations = [augs], mode='train', labels=birds, augment_prob=aug_prob)
+            val_data = SpecDataset(df_val, SPEC_PATH, mode='train', labels=birds) 
+        else:    
+            train_data = SpecDataset(df_train, SPEC_PATH, mode='train', labels=birds)
+            val_data = SpecDataset(df_val, SPEC_PATH, mode='train', labels=birds)  
+
+    else:
+        if augs != '':
+            print('\n Warning: Augmentations cannot be applied if precomputed is not activated \n')
+
+        train_data = SimpleDataset(df_train, DATA_PATH, mode='train', labels=birds)
+        val_data = SimpleDataset(df_val, DATA_PATH, mode='train', labels=birds)
+
+
+    train_selector = Selector(duration=max_duration, offset=offset_train, device='cpu') 
+
+    train_loader = DataLoader(
+        train_data, 
+        batch_size=bs_train, 
+        num_workers=8, #8
+        collate_fn=lambda x: collate_fn(x, load_all=False, sr=sr, duration=max_duration, selector=train_selector), # defined in train_utils.py
+        shuffle=True, 
+        pin_memory=True
+    )
+    val_loader = DataLoader(
+        val_data, 
+        batch_size=bs_val, 
+        num_workers=8, #8
+        collate_fn=lambda x: collate_fn(x, load_all=True), # defined in train_utils.py
+        shuffle=False, 
+        pin_memory=True
+    )
+
+
+    # create model
+    transforms1_train = [ 
+            SelectSplitData(duration, n_splits, offset=offset_train, sr=sr)
+            # add more transforms here
+        ]
+    transforms1_train = TransformApplier(
+        check_args(transforms1_train, args, keep='SelectSplitData')
+    )
+
+    transforms1_val = [ 
+            SelectSplitData(duration, n_splits, offset=offset_val, sr=sr), 
+            # add more transforms here
+        ]
+    transforms1_val = TransformApplier(
+        check_args(transforms1_val, args, keep='SelectSplitData')
+    )
+
+    augment = [
+            tam.Gain(
+                min_gain_in_db=-15.0,
+                max_gain_in_db=5.0,
+                p=0.5
+            ),
+            tam.PolarityInversion(p=0.5)
+        ]
+
+
+    transforms2 = [ 
+        InstanceNorm()
+    ]
+
+    transforms2 = TransformApplier(
+        check_args(transforms2, args)
+    )
+
+
+    wav2spec = nn.Identity() if precompute else Wav2Spec()
+
+    data_pipeline_train = nn.Sequential(
+        transforms1_train, 
+        wav2spec,
+        transforms2, 
+    ).to(device)
+
+    data_pipeline_val = nn.Sequential(
+        transforms1_val, 
+        wav2spec,
+        transforms2
+    ).to(device) 
+
+    # Model Architecture
+    cnn = PretrainedModel(
+        model_name=model_name, 
+        in_chans=1, # normally 3 for RGB-images
+    )
+
+    # Post-processing
+    transforms3 = [
+            SimpleAttention(cnn.get_out_dim()), 
+            RejoinSplitData(duration, n_splits, sr=sr)
+        ]
+    transforms3 = TransformApplier(
+        check_args(transforms3, args, keep=['SimpleAttention', 'RejoinSplitData'])
+    )
+
+    output_head = OutputHead(n_in=cnn.get_out_dim() * n_splits, n_out=num_classes)
+
+    # Model definition
+    model = nn.Sequential(
+        cnn,
+        transforms3, 
+        output_head,
+    ).to(device)
+
+    optimizer = optimizer(model.parameters(), lr=learning_rate)
+
+    metric_f1micro = MultilabelF1Score(
+        num_labels = num_classes, 
+        topk = 1, # this means we say that we take the label with the highest probability for prediction
+        average='micro', 
+    ).to(device) 
+
+    metric_f1_ours = F1Macro() # PickyScore(MultilabelF1Score)
+    metric_recall_ours = RecallMacro() # PickyScore(MultilabelRecall)
+    metric_prec_ours = PrecisionMacro() # PickyScore(MultilabelPrecision)
+    #metric_f1_old = PickyScore(MultilabelF1Score)
+    #metric_recall_old = PickyScore(MultilabelRecall)
+    #metric_prec_old = PickyScore(MultilabelPrecision).to(device)
+
+    metrics = {
+                'F1Micro': metric_f1micro,
+                'F1Ours': metric_f1_ours,
+                'RecallOurs': metric_recall_ours,
+                'PrecisionOurs': metric_prec_ours,
+                #'F1_old': metric_f1_old,
+                #'Recall_old': metric_recall_old,
+                #'Precision_old': metric_prec_old,
+            }
+
+    model_saver = ModelSaver(OUTPUT_DIR, experiment_name)
+
+    policies = {
+        'first_and_final': first_and_final,
+        'max_thresh': max_thresh, 
+        'max_all': max_all    
+    }
+    policy = policies[args.policy]
+
+    validator = Validator(
+        data_pipeline_val, 
+        model, 
+        overlap=overlap, 
+        device=device, 
+        policy=policy,
+        scheme=args.scheme
+    )
+
+    metrics = [
+        Metric(name, method) for name, method in metrics.items()
+    ]
+
+    config = {
+        "epochs": epochs,
+        "batch_size_train": bs_train,
+        "batch_size_val": bs_val,
+        "learning_rate": learning_rate,
+        "device": device,
+        "duration" : duration,
+        "n_splits" : n_splits,
+        "overlap": overlap, 
+        "transforms1_train": transforms1_train,
+        "transforms1_val": transforms1_val,
+        "transforms2": transforms2,
+        "transforms3": transforms3,
+        "model": model,
+        "test_split" : test_split, 
+        "args": args
+    }
+
+    trainer = Trainer(
+        model=model, 
+        data_pipeline_train=data_pipeline_train, 
+        data_pipeline_val=data_pipeline_val, 
+        model_saver=model_saver,
+        validator=validator,
+        criterion=criterion, 
+        optimizer=optimizer, 
+        device=device, 
+        metrics=metrics, 
+        validate_every=validate_every, 
+        use_wandb=use_wandb, 
+        wandb_args={
+            'columns': ['Predicted', 'Expected'], 
+            'project_name': project_name, 
+            'experiment_name': experiment_name, 
+            'config': config, 
+        }
+    )
+
+    trainer.train(
+        train_loader, 
+        val_loader, 
+        epochs=epochs
+    )
 
 
 def main():
     args = parse_args()
     print(args)
-    if args.k_runs not in [1,3]:
-        raise Exception(f'k_runs = {args.k_runs} is currently not supported. Choose either 1 or 3')
 
     for k in range(args.k_runs):
-        print(f'\n########################## Running {k} split ###############################')
-        experiment_name = args.experiment_name
-        # Take args defined in parse_args(): 
-        duration = args.duration 
-        max_duration = args.max_duration
-        n_splits = args.n_splits
-        test_split = args.test_split 
-        sr = args.sr 
-        offset_train = args.offset_train 
-        offset_val = args.offset_val
-
-        bs_train = args.batch_size_train
-        bs_val = args.batch_size_val
-        precompute = args.precompute
-        augs = args.augs
-        aug_prob = args.aug_prob
-
-        epochs = args.epochs 
-        learning_rate = args.learning_rate
-        device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        model_name = args.model_name
-
-        # Here, to use the args, define a dict
-        # containing losses available, and access them with the
-        # key args.loss
-
-
-        losses = {'BCELoss':nn.BCELoss(),'FocalLoss':FocalLoss(), 'WeightedBCELoss':WeightedBCELoss(),'WeightedFocalLoss':WeightedFocalLoss()}
-
-
-        criterion = losses[args.loss]
-
-        # Similarly define optimizers as losses
-        optimizers = {'Adam': Adam}
-        optimizer = optimizers[args.optimizer]
-
-        overlap = args.overlap
-        validate_every = args.validate_every
-        use_wandb = args.wandb
-        project_name = args.project_name 
-
-        N = args.N 
-
-        # From this point on, it is a regular train function
-        if N != -1:
-            warnings.warn(f'\n\nWarning! Using only {N} training examples!\n')
-
-        # Load data 
-        with open(f'{DATA_PATH}all_birds.json') as f:
-            birds = json.load(f)
-
-        num_classes = len(birds)
-
-        if args.k_runs == 1:
-            df_train = pd.read_csv(f'{SPLIT_PATH}train_metadata.csv')[:N]
-            df_val = pd.read_csv(f'{SPLIT_PATH}val_metadata.csv')[:N]
-        elif args.k_runs == 3:
-            df_train = pd.read_csv(f'{SPLIT_PATH_KFOLD}{k}/train_metadata.csv')[:N]
-            df_val = pd.read_csv(f'{SPLIT_PATH_KFOLD}{k}/val_metadata.csv')[:N]
-
-        # Datasets, DataLoaders
-        if precompute:
-            if augs != '':
-                train_data = AugmentDataset(df_train, SPEC_PATH, AUGMENT_PATH, augmentations = [augs], mode='train', labels=birds, augment_prob=aug_prob)
-                val_data = SpecDataset(df_val, SPEC_PATH, mode='train', labels=birds) 
-            else:    
-                train_data = SpecDataset(df_train, SPEC_PATH, mode='train', labels=birds)
-                val_data = SpecDataset(df_val, SPEC_PATH, mode='train', labels=birds)  
-
-        else:
-            if augs != '':
-                print('\n Warning: Augmentations cannot be applied if precomputed is not activated \n')
-
-            train_data = SimpleDataset(df_train, DATA_PATH, mode='train', labels=birds)
-            val_data = SimpleDataset(df_val, DATA_PATH, mode='train', labels=birds)
-    
-
-        train_selector = Selector(duration=max_duration, offset=offset_train, device='cpu') 
-
-        train_loader = DataLoader(
-            train_data, 
-            batch_size=bs_train, 
-            num_workers=8, #8
-            collate_fn=lambda x: collate_fn(x, load_all=False, sr=sr, duration=max_duration, selector=train_selector), # defined in train_utils.py
-            shuffle=True, 
-            pin_memory=True
-        )
-        val_loader = DataLoader(
-            val_data, 
-            batch_size=bs_val, 
-            num_workers=8, #8
-            collate_fn=lambda x: collate_fn(x, load_all=True), # defined in train_utils.py
-            shuffle=False, 
-            pin_memory=True
-        )
-
-
-        # create model
-        transforms1_train = [ 
-                SelectSplitData(duration, n_splits, offset=offset_train, sr=sr)
-                # add more transforms here
-            ]
-        transforms1_train = TransformApplier(
-            check_args(transforms1_train, args, keep='SelectSplitData')
-        )
-
-        transforms1_val = [ 
-                SelectSplitData(duration, n_splits, offset=offset_val, sr=sr), 
-                # add more transforms here
-            ]
-        transforms1_val = TransformApplier(
-            check_args(transforms1_val, args, keep='SelectSplitData')
-        )
-
-        augment = [
-                tam.Gain(
-                    min_gain_in_db=-15.0,
-                    max_gain_in_db=5.0,
-                    p=0.5
-                ),
-                tam.PolarityInversion(p=0.5)
-            ]
-        
-
-        transforms2 = [ 
-            InstanceNorm()
-        ]
-        
-        transforms2 = TransformApplier(
-            check_args(transforms2, args)
-        )
-
-        
-        wav2spec = nn.Identity() if precompute else Wav2Spec()
-
-        data_pipeline_train = nn.Sequential(
-            transforms1_train, 
-            wav2spec,
-            transforms2, 
-        ).to(device)
-
-        data_pipeline_val = nn.Sequential(
-            transforms1_val, 
-            wav2spec,
-            transforms2
-        ).to(device) 
-
-        # Model Architecture
-        cnn = PretrainedModel(
-            model_name=model_name, 
-            in_chans=1, # normally 3 for RGB-images
-        )
-
-        # Post-processing
-        transforms3 = [
-                SimpleAttention(cnn.get_out_dim()), 
-                RejoinSplitData(duration, n_splits, sr=sr)
-            ]
-        transforms3 = TransformApplier(
-            check_args(transforms3, args, keep=['SimpleAttention', 'RejoinSplitData'])
-        )
-
-        output_head = OutputHead(n_in=cnn.get_out_dim() * n_splits, n_out=num_classes)
-
-        # Model definition
-        model = nn.Sequential(
-            cnn,
-            transforms3, 
-            output_head,
-        ).to(device)
-
-        optimizer = optimizer(model.parameters(), lr=learning_rate)
-
-        metric_f1micro = MultilabelF1Score(
-            num_labels = num_classes, 
-            topk = 1, # this means we say that we take the label with the highest probability for prediction
-            average='micro', 
-        ).to(device) 
-
-        metric_f1_ours = F1Macro() # PickyScore(MultilabelF1Score)
-        metric_recall_ours = RecallMacro() # PickyScore(MultilabelRecall)
-        metric_prec_ours = PrecisionMacro() # PickyScore(MultilabelPrecision)
-        #metric_f1_old = PickyScore(MultilabelF1Score)
-        #metric_recall_old = PickyScore(MultilabelRecall)
-        #metric_prec_old = PickyScore(MultilabelPrecision).to(device)
-        
-        metrics = {
-                    'F1Micro': metric_f1micro,
-                    'F1Ours': metric_f1_ours,
-                    'RecallOurs': metric_recall_ours,
-                    'PrecisionOurs': metric_prec_ours,
-                    #'F1_old': metric_f1_old,
-                    #'Recall_old': metric_recall_old,
-                    #'Precision_old': metric_prec_old,
-                }
-        
-        model_saver = ModelSaver(OUTPUT_DIR, experiment_name)
-
-        policies = {
-            'first_and_final': first_and_final,
-            'max_thresh': max_thresh, 
-            'max_all': max_all    
-        }
-        policy = policies[args.policy]
-
-        validator = Validator(
-            data_pipeline_val, 
-            model, 
-            overlap=overlap, 
-            device=device, 
-            policy=policy,
-            scheme=args.scheme
-        )
-
-        metrics = [
-            Metric(name, method) for name, method in metrics.items()
-        ]
-
-        config = {
-            "epochs": epochs,
-            "batch_size_train": bs_train,
-            "batch_size_val": bs_val,
-            "learning_rate": learning_rate,
-            "device": device,
-            "duration" : duration,
-            "n_splits" : n_splits,
-            "overlap": overlap, 
-            "transforms1_train": transforms1_train,
-            "transforms1_val": transforms1_val,
-            "transforms2": transforms2,
-            "transforms3": transforms3,
-            "model": model,
-            "test_split" : test_split, 
-            "args": args
-        }
-
-        trainer = Trainer(
-            model=model, 
-            data_pipeline_train=data_pipeline_train, 
-            data_pipeline_val=data_pipeline_val, 
-            model_saver=model_saver,
-            validator=validator,
-            criterion=criterion, 
-            optimizer=optimizer, 
-            device=device, 
-            metrics=metrics, 
-            validate_every=validate_every, 
-            use_wandb=use_wandb, 
-            wandb_args={
-                'columns': ['Predicted', 'Expected'], 
-                'project_name': project_name, 
-                'experiment_name': experiment_name, 
-                'config': config, 
-            }
-        )
-        
-        trainer.train(
-            train_loader, 
-            val_loader, 
-            epochs=epochs
-        )
+        if args.k_runs > 1:
+            print('#' * 20 + f'\nRunning {k} split\n' + '#' * 20)
+        train(args, k)
 
 
 if __name__ == '__main__':
